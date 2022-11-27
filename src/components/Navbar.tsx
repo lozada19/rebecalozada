2b126c76b90659775b9fff34a9ff392321650fcd
rebeca <reb19loza@gmail.com>
2022-11-27 22:44:03 +0100

    detalles listos

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,64 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import Icon from "../images/burger-menu.png";
 
 function Navbar() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
 
   const scrollAnimado = (elemento: any) => {
+    closeMenu()
     const destino = document.querySelector(elemento);
     destino.scrollIntoView({
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   };
 
   return (
     <header>
-      <div className="container">
-        <p className="logo">RebLoza</p>
-        <nav>
-          <button onClick={ ()=> scrollAnimado("#Home")  }>Inicio</button>
-          <button onClick={ ()=> scrollAnimado("#about-me")}>Sobre mi</button>
-          <button onClick={ ()=> scrollAnimado("#my-studies")}>Estudios </button>
-          <button onClick={ ()=> scrollAnimado("#my-project")}>Proyectos</button>
-        </nav>
+      <div className="menu-navbar">
+        <div className="navbar-logo">
+          <img className="botton-burger" onClick={handleToggle} src={Icon}/>
+
+          <div className="logo2" />
+          <p className="logo">RebLoza</p>
+        </div>
+        
+
+        <ul className={`${navbarOpen ? "showMenu" : "navBurger"}`}>
+          <button className="button-nav" onClick={() => scrollAnimado("#Home")} >
+            Inicio
+          </button>
+          <hr className="solid" />
+          <button
+            className="button-nav"
+            onClick={() => scrollAnimado("#about-me")}
+          >
+            Sobre mi
+          </button>
+          <hr className="solid" />
+          <button
+            className="button-nav"
+            onClick={() => scrollAnimado("#my-studies")}
+          >
+            Estudios{" "}
+          </button>
+          <hr className="solid" />
+          <button
+            className="button-nav"
+            onClick={() => scrollAnimado("#my-project")}
+          >
+            Proyectos
+          </button>
+          <hr className="solid" />
+        </ul>
       </div>
     </header>
   );
